1367af7f258999a5f62af3d2695730ba5d3f41db
BansariPatel96 <46641027+BansariPatel96@users.noreply.github.com>
2019-03-18 14:25:19 -0400

    Added Google API Auth

--- LibraryManagementSystem/App_Start/Startup.Auth.cs
+++ LibraryManagementSystem/App_Start/Startup.Auth.cs
@@ -6,6 +6,7 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
 using LibraryManagementSystem.Models;
+using System.Configuration;
 
 namespace LibraryManagementSystem
 {
@@ -58,11 +59,11 @@ namespace LibraryManagementSystem
             //   appId: "",
             //   appSecret: "");
 
-            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
-            //{
-            //    ClientId = "",
-            //    ClientSecret = ""
-            //});
+            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            {
+                ClientId = ConfigurationManager.AppSettings["GoogleClientID"],
+                ClientSecret = ConfigurationManager.AppSettings["GoogleClientSecret"]
+            });
         }
     }
 }
