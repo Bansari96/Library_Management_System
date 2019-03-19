a35e111aab1fc8123965fb67194183792b4747c1
BansariPatel96 <46641027+BansariPatel96@users.noreply.github.com>
2019-03-19 12:34:26 -0400

    Forced SSL Request and Fix Social Auth

--- LibraryManagementSystem/App_Start/FilterConfig.cs
+++ LibraryManagementSystem/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace LibraryManagementSystem
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
