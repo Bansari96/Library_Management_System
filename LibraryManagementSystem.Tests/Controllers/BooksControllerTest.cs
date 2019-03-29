ddb18c0e94de42c83e90d18691f23453dd3e8816
BansariPatel96 <46641027+BansariPatel96@users.noreply.github.com>
2019-03-29 14:43:35 -0400

    BooksControllerTest created and Moq installed

---
+++ LibraryManagementSystem.Tests/Controllers/BooksControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LibraryManagementSystem.Tests.Controllers
+{
+    [TestClass]
+    public class BooksControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
